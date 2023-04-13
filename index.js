008c93ca7aed58344e23fa82bfdf40791ac3a176
DESKTOP-LG6H19B\jlewi <jlewis@vogcalgaryappdeveloper.com>
2023-04-13 14:57:15 -0600

    Updated welcome message again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Peeps!')
+  res.send('A new welcome from LaunchPad by Vog!')
 })
 
 app.listen(port, () => {
